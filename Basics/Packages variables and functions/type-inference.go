03f6ec07c7478a79b813d30bbd62102b5da9b351
Sai Sanjay Kottakota <97831658+sanjay7178@users.noreply.github.com>
2024-01-14 15:56:34 +0000

    Added files via upload

---
+++ Basics/Packages variables and functions/type-inference.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	v := 42 // change me!
+	f :=  3.1142
+	g := 0.864 +  0.5i
+	fmt.Printf("v is of type %T\n", v)
+	fmt.Printf("v is of type %T\n", f)
+	fmt.Printf("v is of type %T\n", g)
+}
